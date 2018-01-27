e519297e91186698f09f0686fdc8108f4f0d6b10
xwp <xwp_fullstack@163.com>
2018-01-27 15:50:41 +0800

    集成mysql

--- manage.py
+++ manage.py
@@ -1,14 +1,18 @@
 # _*_ coding:utf-8 _*_
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
 class Config(object):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/iHome'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 
 @app.route('/')
