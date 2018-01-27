1723826bcd79d0a882394c4ce6fb9da7712ae456
xwp <xwp_fullstack@163.com>
2018-01-27 18:57:34 +0800

    开启CSRF保护

--- manage.py
+++ manage.py
@@ -3,11 +3,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import redis
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
 
 class Config(object):
+    SECRET_KEY = 'gsevorhwriogh4o3iht4i'
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/iHome'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -18,8 +20,10 @@ class Config(object):
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 redisstorge = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
+CSRFProtect(app)
 
-@app.route('/')
+
+@app.route('/',methods=['GET',"POST"])
 def index():
     return "index"
 
